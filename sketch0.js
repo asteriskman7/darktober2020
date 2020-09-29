4b7e36ebd44a28170ea5d44069e141e806b09c3f
asteriskman <asteriskman7@users.noreply.github.com>
2020-09-29 14:51:03 -0400

    change sketch draw function arguments

--- sketch0.js
+++ sketch0.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class Sketch0 extends Sketch {
-  draw(ctx) {
-    ctx.clearRect(0, 0, this.width, this.height);
+  draw(ctx, width, height, t) {
+    ctx.clearRect(0, 0, width, height);
     ctx.font = '20px Grandstander';
     ctx.fillStyle = 'orange';
     ctx.fillText('Please select a sketch from the list above', 10, 30);
